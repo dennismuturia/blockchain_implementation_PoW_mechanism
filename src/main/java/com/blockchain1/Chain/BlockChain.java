2900372fe376c6aa42a1654a72a5dbf4efb5283b
Dennis Muturia <dmuturia2922@gmail.com>
2019-07-25 11:31:23 +0300

    Implementing the blockchain class that declaires a list of blocks as block chains

---
+++ src/main/java/com/blockchain1/Chain/BlockChain.java
@@ -0,0 +1,11 @@
+package com.blockchain1.Chain;
+
+import com.blockchain1.Block.Block;
+
+import java.util.ArrayList;
+
+public class BlockChain {
+    //Th e blockchain is implemented as an arraylist of blocks
+    public static ArrayList<Block> blockChain = new ArrayList<>();
+    public static ArrayList<String> data = new ArrayList<>();
+}
