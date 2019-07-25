4cfba3cad77a593baa39d97baa6e3c1164edf724
Dennis Muturia <dmuturia2922@gmail.com>
2019-07-25 11:34:48 +0300

    Declared the genesis block. And since it does not have any previous hash then we will hardcode the prevous hash to zero as seen from the return

--- src/main/java/com/blockchain1/Chain/BlockChain.java
+++ src/main/java/com/blockchain1/Chain/BlockChain.java
@@ -8,4 +8,9 @@ public class BlockChain {
     //Th e blockchain is implemented as an arraylist of blocks
     public static ArrayList<Block> blockChain = new ArrayList<>();
     public static ArrayList<String> data = new ArrayList<>();
+
+    //Create the first block called the genesis block]
+    public static Block createGenesisBlock(){
+        return new Block(0, "\"Note that this is the beginning of the block\"", "0");
+    }
 }
