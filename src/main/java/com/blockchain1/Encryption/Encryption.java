15379dd71dfd3291cba97bbd9dd7c039a4f467db
Dennis Muturia <dmuturia2922@gmail.com>
2019-07-25 12:24:14 +0300

    Changed the for loop in the encryption class to loop through the bytehash

--- src/main/java/com/blockchain1/Encryption/Encryption.java
+++ src/main/java/com/blockchain1/Encryption/Encryption.java
@@ -1,21 +1,23 @@
 package com.blockchain1.Encryption;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Encryption {
     public static String sha256(String origin){
         MessageDigest digest = null;
         try {
             digest = MessageDigest.getInstance("SHA-256");
-        }catch (Exception e){
+        }catch (NoSuchAlgorithmException e){
             e.printStackTrace();
         }
 
+        assert digest != null;
         byte[] byteHash = digest.digest(origin.getBytes(StandardCharsets.UTF_8));
 
         StringBuffer hexHash = new StringBuffer();
 
-        for (int i = 0; i <hexHash.length() ; i++) {
+        for (int i = 0; i <byteHash.length ; i++) {
             String hexDigit = Integer.toHexString(0xff & byteHash[i]);
 
             if (hexDigit.length() == 1){
